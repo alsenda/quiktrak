36f229747ce4dc50005858d8f7b9f1b646f0e66a
Álvaro Sendón Aleu <alvaro.sendon.aleu@gmail.com>
2026-02-20 13:00:25 +0100

    remove usememo. React now has a compiler

--- apps/web/src/features/movies/pages/MoviesPage.tsx
+++ apps/web/src/features/movies/pages/MoviesPage.tsx
@@ -1,35 +1,33 @@
-import { use, useMemo } from "react";
+import { use } from "react";
 
 import { getMoviesResource } from "../../../lib/resources";
 import { MoviesToolbar } from "../components/MoviesToolbar";
 import { MoviesGrid } from "../components/MoviesGrid";
 import { MoviesPagination } from "../components/MoviesPagination";
 import { useMoviesPageState } from "../hooks/useMoviesPageState";
-import { compareMovies, toggleSortState } from "../utils/sort";
+import { compareMovies, SortState, toggleSortState } from "../utils/sort";
 import { buildMoviesBackPath } from "../utils/searchParams";
+import { Movie } from "../../../../../../packages/contract/src/schema";
 
 const ITEMS_PER_PAGE = 12;
+
+const getFilteredMovies = (movies: Movie[], debouncedQuery: string): Movie[] => {
+  if (!debouncedQuery) return movies;
+  const needle = debouncedQuery.toLowerCase();
+  return movies.filter((movie) => movie.title.toLowerCase().includes(needle));
+};
+
+const getOrderedMovies = (movies: Movie[], sort: SortState): Movie[] => {
+  if (!sort) return movies;
+  return [...movies].sort((a, b) => compareMovies(a, b, sort));
+};
 
 export default function MoviesPage() {
   const movies = use(getMoviesResource());
 
   const { queryInput, setQueryInput, debouncedQuery, paging, sort, setSort } = useMoviesPageState(ITEMS_PER_PAGE);
-
-  const filteredMovies = useMemo(() => {
-    if (!debouncedQuery) return movies;
-    const needle = debouncedQuery.toLowerCase();
-    return movies.filter((movie) => movie.title.toLowerCase().includes(needle));
-  }, [debouncedQuery, movies]);
-
-  const orderedMovies = useMemo(() => {
-    if (!sort) return filteredMovies;
-    return [...filteredMovies].sort((a, b) => compareMovies(a, b, sort));
-  }, [filteredMovies, sort]);
-
-  const totalPages = useMemo(
-    () => Math.max(1, Math.ceil(orderedMovies.length / ITEMS_PER_PAGE)),
-    [orderedMovies.length]
-  );
+  const orderedMovies = getOrderedMovies(getFilteredMovies(movies, debouncedQuery), sort);
+  const totalPages = Math.max(1, Math.ceil(orderedMovies.length / ITEMS_PER_PAGE));
 
   const safeCurrentPage = Math.min(paging.currentPage, totalPages);
   const startIndex = (safeCurrentPage - 1) * ITEMS_PER_PAGE;
